4e6283d4dcf10b6e9660f3a27ac45fd70b75adc1
jpnewman <johnpaul.newman@gmail.com>
2019-04-01 20:27:10 +0100

    Added CheckS3ObjectExists.

--- aws/s3.go
+++ aws/s3.go
@@ -13,9 +13,30 @@ import (
 	"github.com/spf13/viper"
 )
 
+// CheckS3ObjectExists - Check if S3 Object Exists.
+func CheckS3ObjectExists(s *session.Session, s3Key string) bool {
+
+	_, err := s3.New(s).HeadObject(&s3.HeadObjectInput{
+		Bucket: aws.String(viper.GetString("aws.s3_bucket")),
+		Key:    aws.String(s3Key),
+	})
+
+	if err == nil {
+		return true
+	}
+
+	return false
+}
+
+// UploadFileToS3 - Upload File to S3.
 func UploadFileToS3(s *session.Session, node *models.Node) error {
 	fileDir := path.Join(viper.GetString("repo.filestore_path"), node.RepoFilePath)
 	s3Key := path.Join(viper.GetString("aws.s3_key"), node.Repo, node.NodeName)
+
+	if CheckS3ObjectExists(s, s3Key) {
+		node.Uploaded = true
+		return nil
+	}
 
 	// fmt.Printf("Uploading file to S3: %s", fileDir)
 
